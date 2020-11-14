1bbf6ebe8ad507e8eac564a6f05263bc91e4dca1
mateus <mateussergio84>
2020-11-13 21:18:06 -0300

    configuração tela

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,6 @@ void main() {
   runApp(HomePage());
 }
 
-Widget barraSuperior(
-    {@required String titulo, double height = 100.0}) {
-  return Container(
-      padding: EdgeInsets.all(35.0),
-      color: Colors.blue[700],
-      height: height,
-      child: Center(
-        child: Row(
-          children: [
-            Text(
-              titulo,
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ));
-}
 
 class HomePage extends StatefulWidget {
   @override
@@ -38,92 +17,12 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       routes: {
         '/tela_inicial' : (context) => Tela_inicial(),
-        '/menu': (context) =>
-            TemplatePage(titulo: 'Estoque - Menu', conteudo: Menu()),
-        '/listaProdutos': (context) =>
-            TemplatePage(titulo: 'Estoque', conteudo: ListaProdutos()),
-        '/secundaria': (context) =>
-            TemplatePage(titulo: 'Estoque - Cadastro', conteudo: TelaSecundaria()),
+        '/tela_secundaria' : (context) => TelaSecundaria(),
       },
-      initialRoute: '/tela_inicial',
+      initialRoute: '/tela_secundaria',
     );
   }
 }
-
-class TemplatePage extends StatelessWidget {
-  final String titulo;
-  final Widget conteudo;
-  final double height;
-
-
-  TemplatePage(
-      {this.titulo, this.conteudo, this.height = 100.0});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-          children: [
-            barraSuperior(titulo: titulo, height: height),
-            conteudo
-          ],
-        ));
-  }
-}
-
-
-class ListaProdutos extends StatefulWidget {
-  @override
-  _ListaProdutosState createState() => _ListaProdutosState();
-}
-
-class _ListaProdutosState extends State<ListaProdutos> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          body: Center(
-
-          ),
-        )
-
-    );
-  }
-}
-
-class Menu extends StatefulWidget {
-  @override
-  _Menu createState() => _Menu();
-}
-
-class _Menu extends State<Menu> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.add_shopping_cart),
-          title: Text('Cadastrar Produto'),
-          onTap: () {
-            Navigator.pushNamed(context, '/secundaria');
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.format_list_bulleted_sharp),
-          title: Text('Lista de Produtos'),
-        ),
-        ListTile(
-          leading: Icon(Icons.bar_chart_sharp),
-          title: Text('Controle de gastos'),
-        ),
-      ],
-    );
-  }
-}
-
-
-
-
 
 
 class TelaSecundaria extends StatefulWidget {
@@ -136,41 +35,37 @@ class _TelaSecundariaState extends State<TelaSecundaria> {
   final TextEditingController _controladorQuantidade = TextEditingController();
   final TextEditingController _controladorPreco = TextEditingController();
   final TextEditingController _controladorMinimo = TextEditingController();
-
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: <Widget>[
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Estoque - Menu'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
           TextField(
             controller: _controladorProduto,
-            decoration: InputDecoration(labelText: 'Produto'),
+            decoration: InputDecoration(hintText: 'Produto'),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: TextField(
+           TextField(
               controller: _controladorQuantidade,
               decoration: InputDecoration(labelText: 'Quantidade'),
               keyboardType: TextInputType.number,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: TextField(
+            TextField(
               controller: _controladorPreco,
               decoration: InputDecoration(labelText: 'Preço'),
               keyboardType: TextInputType.number,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: TextField(
+          TextField(
               controller: _controladorMinimo,
               decoration: InputDecoration(labelText: 'Quantidade Minima'),
               keyboardType: TextInputType.number,
             ),
-          ),
-
           FlatButton(
             color: Colors.blue[700],
             child: Text('Adicionar',
@@ -183,8 +78,8 @@ class _TelaSecundariaState extends State<TelaSecundaria> {
             onPressed: (){
             },
           ),
-
         ]
+        ),
     );
   }
 }
@@ -203,6 +98,7 @@ class _Tela_inicialState extends State<Tela_inicial> {
         appBar: AppBar(
           centerTitle: true,
           title: Text('Estoque - Menu'),
+          backgroundColor: Colors.indigo,
         ),
         body:
         ListView(
